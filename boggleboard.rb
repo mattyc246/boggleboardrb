1561aff460d04d1316912de255f490408d833b2e
mattyc246 <matty_c_246@gmail.com>
2018-11-13 14:59:59 +0800

    Testing updates and functions added to boggle class

--- boggleboard.rb
+++ boggleboard.rb
@@ -1,3 +1,12 @@
+########## Boggle Board ##########
+### Thought Process
+
+# First initilize the boggle board with the dice given
+# Start a new round which will result in shaking the dice and printing out the board
+# Take in the user input of a word guess
+# Find the indexes of all the beginning letters of the word
+# Go and 
+
 class Boggle
 
 	attr_reader :dice
@@ -6,7 +15,46 @@ class Boggle
 		@dice = starting_dice
 	end
 
+	def play_round
+
+	end
+
+	def shake_dice!
+
+		dice = @dice.shuffle
+
+		board = []
+
+		dice.each do |die|
+
+			board << die.sample
+
+		end
+
+		return board
+
+
+	end
+
+	def print_board(board)
+
+		puts ""
+		puts "================="
+
+		board.each_slice(4) do |row|
+
+			puts "| #{row[0]} | #{row[1]} | #{row[2]} | #{row[3]} |"
+			puts "================="
+
+		end
+
+		puts ""
+
+	end
+
 end
+
+########### Driver Code ###########
 
 dice = [['A','A','E','E','G','N'],
 		['E','L','R','T','T','Y'],
@@ -24,3 +72,7 @@ dice = [['A','A','E','E','G','N'],
 		['A','F','F','K','P','S'],
 		['H','L','N','N','R','Z'],
 		['D','E','I','L','R','X']]
+
+boggle = Boggle.new(dice)
+board = boggle.shake_dice!
+boggle.print_board(board)
--- boggleboard_spec.rb
+++ boggleboard_spec.rb
@@ -3,5 +3,30 @@ require 'pry'
 require_relative './boggleboard'
 
 Rspec.describe Boggle do
-	
+
+	Given(:dice) { [['A','A','E','E','G','N'],['E','L','R','T','T','Y'],['A','O','O','T','T','W'],['A','B','B','J','O','O'],['E','H','R','T','V','W'],['C','I','M','O','T','U'],['D','I','S','T','T','Y'],['E','I','O','S','S','T'],['D','E','L','R','V','Y'],['A','C','H','O','P','S'],['H','I','M','N','Q','U'],['E','E','I','N','S','U'],['E','E','G','H','N','W'],['A','F','F','K','P','S'],['H','L','N','N','R','Z'],['D','E','I','L','R','X']] }
+	Given(:boggle) { Boggle.new(dice) }
+	Given (:board) { ['T','A','E','O','E','R','U','T','I','R','S','H','K','R','I','T'] }
+
+	context '#initialize' do
+		When(:results) { boggle.dice }
+		Then { results == dice }
+	end
+
+	context '#shake_dice!' do
+		When(:results) { boggle.shake_dice! }
+		Then { results.count == 16 }
+	end
+
+	context '#find_starting_letters' do
+		Given (:word) { "RAT" }
+		When (:results) { boggle.find_starting_letters(board, word) }
+		Then { results == [5,9,13] }
+	end
+
+	context '#find_surrounding' do
+		Given (:letter) { "R" }
+		When (:results) { boggle.find_surrounding(board, current_letter) }
+		Then { results == ['T','A','E','E','U','I','R','S'] }
+	end
 end
