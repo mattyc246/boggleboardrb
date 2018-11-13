bfb19ff2a996b4b4b604ca7f934b4f950778ded1
mattyc246 <matty_c_246@gmail.com>
2018-11-13 11:01:24 +0800

    first test written for initialize

--- boggleboard.rb
+++ boggleboard.rb
@@ -1,7 +1,26 @@
 class Boggle
 
-	def initialize
-		
+	attr_reader :dice
+
+	def initialize(starting_dice)
+		@dice = starting_dice
 	end
 
 end
+
+dice = [['A','A','E','E','G','N'],
+		['E','L','R','T','T','Y'],
+		['A','O','O','T','T','W'],
+		['A','B','B','J','O','O'],
+		['E','H','R','T','V','W'],
+		['C','I','M','O','T','U'],
+		['D','I','S','T','T','Y'],
+		['E','I','O','S','S','T'],
+		['D','E','L','R','V','Y'],
+		['A','C','H','O','P','S'],
+		['H','I','M','N','Q','U'],
+		['E','E','I','N','S','U'],
+		['E','E','G','H','N','W'],
+		['A','F','F','K','P','S'],
+		['H','L','N','N','R','Z'],
+		['D','E','I','L','R','X']]
